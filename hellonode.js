d445bac0f3556305bd6be464b24471bd59fd39d4
Nathaniel Daley <ndaley64@gmail.com>
2024-12-12 23:10:36 -0600

    update filetree

--- hellonode.js
+++ hellonode.js
@@ -1,6 +1,6 @@
-const WarmUpChallenge1 = require("./WarmUpChallenge1");
-const WarmUpChallenge2 = require("./WarmupChallenge2");
-const WarmUpChallenge3 = require("./WarmUpChallenge3");
-const WarmUpChallenge4 = require("./WarmUpChallenge4");
+const WarmUpChallenge1 = require("./Solutions/Warmup/WarmUpChallenge1");
+const WarmUpChallenge2 = require("./Solutions/Warmup/WarmupChallenge2");
+const WarmUpChallenge3 = require("./Solutions/Warmup/WarmUpChallenge3");
+const WarmUpChallenge4 = require("./Solutions/Warmup/WarmUpChallenge4");
 
 WarmUpChallenge4.test();
