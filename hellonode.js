54fd4e25964a08b22e4452bd926fd904175da618
Nathaniel Daley <ndaley64@gmail.com>
2024-12-13 21:05:42 -0600

    add problem descriptions

--- hellonode.js
+++ hellonode.js
@@ -1,6 +1,6 @@
-const WarmUpChallenge1 = require("./Solutions/Warmup/WarmUpChallenge1");
-const WarmUpChallenge2 = require("./Solutions/Warmup/WarmupChallenge2");
-const WarmUpChallenge3 = require("./Solutions/Warmup/WarmUpChallenge3");
-const WarmUpChallenge4 = require("./Solutions/Warmup/WarmUpChallenge4");
+const WarmUpChallenge1 = require("./Solutions/Warmup/Challenge1/WarmUpChallenge1");
+const WarmUpChallenge2 = require("./Solutions/Warmup/Challenge2/WarmupChallenge2");
+const WarmUpChallenge3 = require("./Solutions/Warmup/Challenge3/WarmUpChallenge3");
+const WarmUpChallenge4 = require("./Solutions/Warmup/Challenge4/WarmUpChallenge4");
 
 WarmUpChallenge4.test();
